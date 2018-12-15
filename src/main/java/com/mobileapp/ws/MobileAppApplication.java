850a1bfbe66c8e4b9c79c41eec0f8583b062026e
rajeshmohite <rajehmohite694@gmail.com>
2018-12-15 12:43:41 +0530

    Quick Start

--- src/main/java/com/mobileapp/ws/MobileAppApplication.java
+++ src/main/java/com/mobileapp/ws/MobileAppApplication.java
@@ -1,4 +1,4 @@
-package com.mobileapp.ws.MobileApp;
+package com.mobileapp.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/mobileapp/ws/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.mobileapp.ws.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+	@GetMapping
+	public String getUser() {
+		return "get user";
+	}
+
+	@PostMapping
+	public String createUser() {
+		return "create user";
+	}
+
+	@PutMapping
+	public String updateUser() {
+		return "update user";
+	}
+
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user";
+	}
+
+}
