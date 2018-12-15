a2134d9f78abd5714de56af80ec408e3f1ef5dc3
rajeshmohite <rajehmohite694@gmail.com>
2018-12-15 12:38:18 +0530

    git init

---
+++ src/test/java/com/mobileapp/ws/MobileApp/MobileAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mobileapp.ws.MobileApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MobileAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
