491f9e8e41c8ddccdab865ba0eb837f10911b5c1
rajeshmohite <rajehmohite694@gmail.com>
2018-12-16 12:31:44 +0530

    Add Exception Handling

--- src/main/java/com/mobileapp/ws/controller/UserController.java
+++ src/main/java/com/mobileapp/ws/controller/UserController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mobileapp.ws.dto.UserDto;
+import com.mobileapp.ws.exceptions.UserServiceException;
 import com.mobileapp.ws.service.UserService;
 import com.mobileapp.ws.ui.model.request.UserDetailsRequestModel;
+import com.mobileapp.ws.ui.model.response.ErrorMessages;
 import com.mobileapp.ws.ui.model.response.UserDetailsResponseModel;
 
 @RestController
@@ -36,8 +38,15 @@ public class UserController {
 
 	@PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = {
 			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public UserDetailsResponseModel createUser(@RequestBody UserDetailsRequestModel userDetails) {
+	public UserDetailsResponseModel createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
 		UserDetailsResponseModel returnUser = new UserDetailsResponseModel();
+
+		if (userDetails.getFirstName().isEmpty())
+			throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+
+		// Below case is just to check how other exceptions can handle
+		if (userDetails.getPassword().isEmpty())
+			throw new NullPointerException("The Object is Null");
 
 		UserDto userDto = new UserDto();
 		BeanUtils.copyProperties(userDetails, userDto);
